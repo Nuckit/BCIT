113bdc7f1b583725ace688e86b4f04bbeee9f52c
Nuckit <leongr@live.com>
2015-07-02 20:58:09 -0700

    Lab9

---
+++ COMP2870/Lab9/MasterPage.master.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    public string CurrentPage
+    {
+        get { return LabelCurrentPage.Text; }
+        set { LabelCurrentPage.Text = value; }
+    }
+     
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
---
+++ COMP2870/Lab9/Page2.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Page2.aspx.cs" Inherits="Page2" Theme="Theme"%>
+<%@ MasterType VirtualPath="MasterPage.master" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    This is Page 2!
+</asp:Content>
+

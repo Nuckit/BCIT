be0c14cbcf8a166ce0b01e7b70c860353dcf4ec4
Nuckit <leongr@live.com>
2015-10-06 20:41:19 -0700

    Week4 Lab

---
+++ COMP3973/Week4/Week4/Models/Partials/Region.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Week4.Models.Northwind
+{
+    [MetadataType(typeof(RegionMetadata))]
+    public partial class Region
+    {
+    }
+
+    public class RegionMetadata
+    {
+        [Required]
+        public object RegionID { get; set; }
+
+        [Required]
+        [Display(Name = "REGION DESCRIPTION")]
+        [StringLength(10, ErrorMessage = "The {0} must be between {2} and {1} characters.", MinimumLength = 1)]
+        public object RegionDescription { get; set; }
+    }
+}

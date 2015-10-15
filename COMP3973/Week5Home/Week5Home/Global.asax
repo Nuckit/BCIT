22bf7da3e91282bc284e754dcdc754ac18f6e294
Nuckit <leongr@live.com>
2015-10-14 23:50:45 -0700

    Week 5 home
    initial

---
+++ COMP3973/Week5Home/Week5Home/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week5Home.MvcApplication" Language="C#" %>
---
+++ COMP3973/Week5Home/Week5Home/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week5Home.Startup))]
+namespace Week5Home
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

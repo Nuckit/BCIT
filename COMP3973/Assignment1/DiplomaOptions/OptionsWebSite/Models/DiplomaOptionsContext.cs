86c7dbc7c715340671897ee3a7c6f8ffdda3bf9a
Nuckit <leongr@live.com>
2015-10-21 20:41:17 -0700

    Added DbContexts

---
+++ COMP3973/Assignment1/DiplomaOptions/OptionsWebSite/Models/DiplomaOptionsContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using DiplomaDataModel;
+
+namespace OptionsWebSite.Models
+{
+    public class DiplomaOptionsContext : DbContext
+    {
+        public DiplomaOptionsContext() : base("DefaultConnection")
+        {            
+        }
+
+        public DbSet<Choice> Choices { get; set; }
+        public DbSet<Option> Options { get; set; }
+        public DbSet<YearTerm> YearTerms { get; set; }
+    }
+}

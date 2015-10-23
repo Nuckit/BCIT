a109ab1eb1cf20662c05da320f39355c4223b746
Nuckit <leongr@live.com>
2015-10-22 23:00:35 -0700

    Added Seed methods

--- COMP3973/Assignment1/DiplomaOptions/OptionsWebSite/Migrations/DiplomaOptionsMigrations/Configuration.cs
+++ COMP3973/Assignment1/DiplomaOptions/OptionsWebSite/Migrations/DiplomaOptionsMigrations/Configuration.cs
@@ -1,3 +1,5 @@
+using DiplomaDataModel;
+
 namespace OptionsWebSite.Migrations.DiplomaOptionsMigrations
 {
     using System;
@@ -27,6 +29,79 @@ namespace OptionsWebSite.Migrations.DiplomaOptionsMigrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            var yearTerm1 = new YearTerm
+            {
+                Year = 2015,
+                Term = 10,
+                IsDefault = false
+            };
+            var yearTerm2 = new YearTerm
+            {
+                Year = 2015,
+                Term = 20,
+                IsDefault = false
+            };
+            var yearTerm3 = new YearTerm
+            {
+                Year = 2015,
+                Term = 30,
+                IsDefault = false
+            };
+            var yearTerm4 = new YearTerm
+            {
+                Year = 2016,
+                Term = 10,
+                IsDefault = true
+            };
+
+            context.YearTerms.AddOrUpdate(y => y.YearTermId, yearTerm1, yearTerm2, yearTerm3, yearTerm4);
+            context.SaveChanges();
+
+            var option1 = new Option
+            {
+                Title = "Data Communications",
+                IsActive = true
+            };
+
+            var option2 = new Option
+            {
+                Title = "Client Server",
+                IsActive = true
+            };
+
+            var option3 = new Option
+            {
+                Title = "Digital Processing",
+                IsActive = true
+            };
+
+            var option4 = new Option
+            {
+                Title = "Information Systems",
+                IsActive = true
+            };
+
+            var option5 = new Option
+            {
+                Title = "Database",
+                IsActive = false
+            };
+
+            var option6 = new Option
+            {
+                Title = "Web & Mobile",
+                IsActive = true
+            };
+
+            var option7 = new Option
+            {
+                Title = "Tech Pro",
+                IsActive = false
+            };
+
+            context.Options.AddOrUpdate(o => o.OptionId, option1, option2, option3, option4, option5, option6, option7);
+            context.SaveChanges();
         }
     }
 }
--- COMP3973/Assignment1/DiplomaOptions/OptionsWebSite/Migrations/IdentityMigrations/Configuration.cs
+++ COMP3973/Assignment1/DiplomaOptions/OptionsWebSite/Migrations/IdentityMigrations/Configuration.cs
@@ -1,11 +1,11 @@
+using System.Data.Entity.Migrations;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using OptionsWebSite.Models;
+
 namespace OptionsWebSite.Migrations.IdentityMigrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-
-    internal sealed class Configuration : DbMigrationsConfiguration<OptionsWebSite.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
@@ -13,7 +13,7 @@ namespace OptionsWebSite.Migrations.IdentityMigrations
             MigrationsDirectory = @"Migrations\IdentityMigrations";
         }
 
-        protected override void Seed(OptionsWebSite.Models.ApplicationDbContext context)
+        protected override void Seed(ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
@@ -27,6 +27,50 @@ namespace OptionsWebSite.Migrations.IdentityMigrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            const string adminRoleName = "Admin";
+            if (!roleManager.RoleExists(adminRoleName))
+            {
+                roleManager.Create(new IdentityRole(adminRoleName));
+            }
+
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            const string defaultAdminUserName = "A00111111";
+            const string defaultAdminEmail = "a@a.a";
+            const string defaultAdminPassword = "P@$$w0rd";
+
+            const string defaultStudentUserName = "A00222222";
+            const string defaultStudentEmail = "s@s.s";
+            const string defaultStudentPassword = "P@$$w0rd";
+
+            if (userManager.FindByName(defaultAdminUserName) == null)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = defaultAdminUserName,
+                    Email = defaultAdminEmail,                    
+                };
+
+                IdentityResult result = userManager.Create(user, defaultAdminPassword);
+                if (result.Succeeded)
+                {
+                    userManager.AddToRole(userManager.FindByName(defaultAdminUserName).Id, adminRoleName);
+                }
+            }
+
+            if (userManager.FindByName(defaultStudentUserName) == null)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = defaultStudentUserName,
+                    Email = defaultStudentEmail,
+                };
+
+                userManager.Create(user, defaultStudentPassword);
+            }
         }
     }
 }

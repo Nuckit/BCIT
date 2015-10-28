061a1d7b1608056e9012464d69957619d35efbc8
Nuckit <leongr@live.com>
2015-10-27 20:19:11 -0700

    Initial week7

---
+++ COMP3973/Week7/Week7/Models/Partials/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using Week7.Models.Validation;
+
+namespace Week7.Models
+{
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {         
+    }
+
+    public class ProductMetaData
+    {
+        [HiddenInput(DisplayValue = false)]
+        public object ProductID { get; set; }
+
+        [Required]
+        [Display(Name = "Product Name")]
+        [MaxWords(3)]
+        public object ProductName { get; set; }
+    }
+}
---
+++ COMP3973/Week7/Week7/Models/Validation/MaxWords.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Week7.Models.Validation
+{
+    public class MaxWords : ValidationAttribute
+    {
+        private uint WordCount { get; set; }
+
+        public MaxWords(uint wordCount) : base ("{0} exceeds the max number of allowed words")
+        {
+            WordCount = wordCount;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string valueAsString = value.ToString();
+                string[] strings = Regex.Split(valueAsString, @"\s+");
+
+                if (strings.Length > WordCount)
+                {
+                    string errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(errorMessage);
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}

ec80e2de9fae28e59c5fc73c943c2461018e4cee
Nuckit <leongr@live.com>
2015-11-03 19:12:14 -0800

    Week8 lab with student entity

---
+++ COMP3973/Week8/StudentWebApiCorsLab/StudentAPI/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentAPI.Models
+{
+    public class Student
+    {
+        public string StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Program { get; set; }
+    }
+}

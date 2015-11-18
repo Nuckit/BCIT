07174602c44448fb93548303c3fba4eff868be63
Nuckit <leongr@live.com>
2015-11-17 22:50:19 -0800

    Ass2
    Added user and role viewmodels

---
+++ COMP3973/Assignment1/DiplomaOptions/OptionsWebSite/Models/RoleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OptionsWebSite.Models
+{
+    public class RoleViewModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+
+        public List<UserViewModel> Users { get; set; }
+    }
+}
---
+++ COMP3973/Assignment1/DiplomaOptions/OptionsWebSite/Models/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OptionsWebSite.Models
+{
+    public class UserViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        public bool LockoutEnabled { get; set; }
+
+        public List<RoleViewModel> Roles { get; set; }
+    }
+}

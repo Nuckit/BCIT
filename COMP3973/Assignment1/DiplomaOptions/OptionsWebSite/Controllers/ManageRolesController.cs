6a8570d0d97d5b5d550678c325983be1c4b27bfb
Nuckit <leongr@live.com>
2015-11-19 01:14:01 -0800

    A2
    Manage users and roles working

--- COMP3973/Assignment1/DiplomaOptions/OptionsWebSite/Controllers/ManageRolesController.cs
+++ COMP3973/Assignment1/DiplomaOptions/OptionsWebSite/Controllers/ManageRolesController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using DiplomaDataModel;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using OptionsWebSite.Models;
 using WebGrease.Css.Extensions;
@@ -66,7 +67,7 @@ namespace OptionsWebSite.Controllers
             {
                 return new HttpNotFoundResult();
             }
-            return View(new RoleViewModel{RoleName = role.Name});
+            return View(new RoleViewModel { RoleName = roleName });
         }
 
         [HttpPost, ActionName("Delete")]
@@ -77,6 +78,128 @@ namespace OptionsWebSite.Controllers
             context.Roles.Remove(role);
             context.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        public ActionResult AddUser(string roleName)
+        {
+            if (roleName == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IdentityRole role = context.Roles.FirstOrDefault(r => r.Name == roleName);
+            if (role == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            var usersNotInRole = context.Users.Where(user => !user.Roles.Any(r => r.RoleId == role.Id));
+
+            ViewBag.UserName = new SelectList(usersNotInRole, "UserName", "UserName");
+            return View(new RoleViewModel { RoleName = roleName });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddUser(RoleViewModel role)
+        {
+            IdentityRole identityRole = context.Roles.FirstOrDefault(r => r.Name == role.RoleName);
+
+            if (identityRole == null)
+            {
+                ViewBag.ErrorMessage = string.Format("Role '{0}' no longer exists", role.RoleName);
+                return View(new RoleViewModel { RoleName = role.RoleName });
+            }
+
+            if (ModelState.IsValid)
+            {
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                IdentityUser user = context.Users.FirstOrDefault(u => u.UserName == role.UserName);
+
+                if (user == null)
+                {
+                    ViewBag.ErrorMessage = string.Format("User '{0}' no longer exists", role.UserName);
+                }
+                else if (userManager.IsInRole(user.Id, role.RoleName))
+                {
+                    ViewBag.ErrorMessage = string.Format("Role '{0}' already has user '{1}'", role.RoleName, role.UserName);
+                }
+                else
+                {
+                    IdentityResult result = userManager.AddToRole(user.Id, role.RoleName);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+
+                    ViewBag.ErrorMessage = string.Format("Could not add user '{0}' to role '{1}'", role.UserName, role.RoleName);
+                }
+            }
+
+            var usersNotInRole = context.Users.Where(user => user.Roles.All(r => r.RoleId != identityRole.Id));
+
+            ViewBag.UserName = new SelectList(usersNotInRole, "UserName", "UserName");
+            return View(new RoleViewModel { RoleName = role.RoleName });
+        }
+
+        public ActionResult RemoveUser(string roleName)
+        {
+            if (roleName == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IdentityRole role = context.Roles.FirstOrDefault(r => r.Name == roleName);
+            if (role == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            var usersInRole = context.Users.Where(user => user.Roles.Any(r => r.RoleId == role.Id));
+
+            ViewBag.UserName = new SelectList(usersInRole, "UserName", "UserName");
+            return View(new RoleViewModel { RoleName = roleName });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RemoveUser(RoleViewModel role)
+        {
+            IdentityRole identityRole = context.Roles.FirstOrDefault(r => r.Name == role.RoleName);
+
+            if (identityRole == null)
+            {
+                ViewBag.ErrorMessage = string.Format("Role '{0}' no longer exists", role.RoleName);
+                return View(new RoleViewModel { RoleName = role.RoleName });
+            }
+
+            if (ModelState.IsValid)
+            {
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                IdentityUser user = context.Users.FirstOrDefault(u => u.UserName == role.UserName);
+
+                if (user == null)
+                {
+                    ViewBag.ErrorMessage = string.Format("User '{0}' no longer exists", role.UserName);
+                }
+                else if (!userManager.IsInRole(user.Id, role.RoleName))
+                {
+                    ViewBag.ErrorMessage = string.Format("User '{0}' is already not in role '{1}'", role.UserName, role.RoleName);
+                }
+                else
+                {
+                    IdentityResult result = userManager.RemoveFromRole(user.Id, role.RoleName);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+
+                    ViewBag.ErrorMessage = string.Format("Could not remove user '{0}' from role '{1}'", role.UserName, role.RoleName);
+                }
+            }
+
+            var usersNotInRole = context.Users.Where(user => user.Roles.All(r => r.RoleId != identityRole.Id));
+
+            ViewBag.UserName = new SelectList(usersNotInRole, "UserName", "UserName");
+            return View(new RoleViewModel { RoleName = role.RoleName });
         }
     }
 }
--- COMP3973/Assignment1/DiplomaOptions/OptionsWebSite/Models/RoleViewModel.cs
+++ COMP3973/Assignment1/DiplomaOptions/OptionsWebSite/Models/RoleViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace OptionsWebSite.Models
 {
@@ -9,6 +10,9 @@ namespace OptionsWebSite.Models
         [Display(Name = "Role Name")]
         public string RoleName { get; set; }
 
-        public List<UserViewModel> Users { get; set; }
+        [Display(Name = "User")]
+        public string UserName { get; set; }
+
+        public List<SelectListItem> UserList { get; set; }
     }
 }
